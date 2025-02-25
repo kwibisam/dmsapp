f154e3dd8276236c20152c2a8c318cf333101c0c
kwibisam <kwibisam04@gmail.com>
2025-02-25 16:15:02 +0200

    removed some files

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const nextConfig = {
-    webpack: (config,  { isServer }) => {
-        config.resolve.alias.canvas = false;
-        if (!isServer) {
-            // Ensure that all imports of 'yjs' resolve to the same instance
-            config.resolve.alias['yjs'] = path.resolve(__dirname, 'node_modules/yjs')
-          }
+    webpack(config) {
         return config;
     },
 };
